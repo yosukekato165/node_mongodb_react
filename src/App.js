facbad3f651544f9cf6388b64c91e06b3b1ff02b
yosukekato165 <padyk165@gmail.com>
2021-11-30 23:48:20 +0900

    add form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [summary, setSummary] = useState("");
+  const [bedrooms, setBedrooms] = useState();
+  const [bathrooms, setBathrooms] = useState();
+
+  const sendPostData = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+    axios
+      .post(`http://localhost:3030`, {
+        name: name,
+        summary: summary,
+        bedrooms: bedrooms,
+        bathrooms: bathrooms,
+      })
+      .then((res) => {
+        console.log("res");
+        console.log(res.data);
+      });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={sendPostData}>
+          <ul>
+            <li>
+              <label for="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                name="user_name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </li>
+            <li>
+              <label for="summary">summary:</label>
+              <input
+                type="text"
+                id="summary"
+                name="summary"
+                onChange={(e) => setSummary(e.target.value)}
+              />
+            </li>
+            <li>
+              <label for="bedrooms">bedrooms:</label>
+              <input
+                type="text"
+                id="bedrooms"
+                name="bedrooms"
+                onChange={(e) => setBedrooms(e.target.value)}
+              />
+            </li>
+            <li>
+              <label for="bathrooms">bathrooms:</label>
+              <input
+                type="text"
+                id="bathrooms"
+                name="bathrooms"
+                onChange={(e) => setBathrooms(e.target.value)}
+              />
+            </li>
+            <li>
+              <button type="submit">メッセージを送信</button>
+            </li>
+          </ul>
+        </form>
       </header>
     </div>
   );
