734cb77ac472ab6e3e1ee745a0ea87bf69d80f75
yosukekato165 <padyk165@gmail.com>
2021-12-03 23:45:51 +0900

    add reference to mongodb

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import axios from "axios";
 
 import logo from "./logo.svg";
 import "./App.css";
+import { set } from "core-js/fn/dict";
 
 function App() {
-  const [name, setName] = useState("");
-  const [summary, setSummary] = useState("");
-  const [bedrooms, setBedrooms] = useState();
-  const [bathrooms, setBathrooms] = useState();
+  const [registerName, setRegisterName] = useState("");
+  const [registerSummary, setRegisterSummary] = useState("");
+  const [registerBedrooms, setRegisterBedrooms] = useState();
+  const [registerBathrooms, setRegisterBathrooms] = useState();
 
-  const sendPostData = (e) => {
+  const [referenceName, setReferenceName] = useState("");
+
+  const [storedName, setStoredName] = useState("");
+  const [storedSummary, setStoredSummary] = useState("");
+  const [storedBedrooms, setStoredBedrooms] = useState();
+  const [storedBathrooms, setStoredBathrooms] = useState();
+
+  const registerData = (e) => {
     e.preventDefault();
     console.log("clicked");
     axios
       .post(
-        `http://localhost:3030`,
+        `http://localhost:3030/register`,
         JSON.stringify({
-          name: name,
-          summary: summary,
-          bedrooms: bedrooms,
-          bathrooms: bathrooms,
+          name: registerName,
+          summary: registerSummary,
+          bedrooms: registerBedrooms,
+          bathrooms: registerBathrooms,
         })
       )
       .then((res) => {
@@ -28,11 +36,31 @@ function App() {
         console.log(res.data);
       });
   };
+
+  const referenceData = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+
+    axios
+      .post(
+        `http://localhost:3030/reference`,
+        JSON.stringify({
+          name: referenceName,
+        })
+      )
+      .then((res) => {
+        setStoredName(res.name);
+        setStoredSummary(res.summary);
+        setStoredBedrooms(res.bedrooms);
+        setStoredBathrooms(res.bathrooms);
+      });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <form onSubmit={sendPostData}>
+        <h2>Register</h2>
+        <form onSubmit={registerData}>
           <ul>
             <li>
               <label for="name">Name:</label>
@@ -40,7 +68,7 @@ function App() {
                 type="text"
                 id="name"
                 name="user_name"
-                onChange={(e) => setName(e.target.value)}
+                onChange={(e) => setRegisterName(e.target.value)}
               />
             </li>
             <li>
@@ -49,7 +77,7 @@ function App() {
                 type="text"
                 id="summary"
                 name="summary"
-                onChange={(e) => setSummary(e.target.value)}
+                onChange={(e) => setRegisterSummary(e.target.value)}
               />
             </li>
             <li>
@@ -58,7 +86,7 @@ function App() {
                 type="text"
                 id="bedrooms"
                 name="bedrooms"
-                onChange={(e) => setBedrooms(e.target.value)}
+                onChange={(e) => setRegisterBedrooms(e.target.value)}
               />
             </li>
             <li>
@@ -67,14 +95,37 @@ function App() {
                 type="text"
                 id="bathrooms"
                 name="bathrooms"
-                onChange={(e) => setBathrooms(e.target.value)}
+                onChange={(e) => setRegisterBathrooms(e.target.value)}
               />
             </li>
             <li>
-              <button type="submit">メッセージを送信</button>
+              <button type="submit">登録する</button>
             </li>
           </ul>
         </form>
+        <h2>Reference</h2>
+        <form onSubmit={referenceData}>
+          <ul>
+            <li>
+              <label for="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                name="user_name"
+                onChange={(e) => setReferenceName(e.target.value)}
+              />
+            </li>
+            <li>
+              <button type="submit">参照する</button>
+            </li>
+          </ul>
+        </form>
+        <ul>
+          <li>{storedName}</li>
+          <li>{storedSummary}</li>
+          <li>{storedBedrooms}</li>
+          <li>{storedBathrooms}</li>
+        </ul>
       </header>
     </div>
   );
